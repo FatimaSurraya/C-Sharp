14d49e53b6b3716e93ad91f96099dbb233f58f0a
Fatima Mughal <fatimamughal651@gmail.com>
2024-02-01 23:29:58 +0500

    Add project files.

---
+++ C-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to be a Successful Developer");
+            Console.WriteLine("I am passionately learning C#");
+            Console.ReadLine();
+        }
+    }
+}
