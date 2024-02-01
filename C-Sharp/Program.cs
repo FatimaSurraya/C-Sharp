e595d3fceaa82b8e93f44253761e25ad79c22f26
Fatima Mughal <fatimamughal651@gmail.com>
2024-02-02 01:02:52 +0500

    // Variables

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -7,6 +7,18 @@
             Console.WriteLine("I want to be a Successful Developer");
             Console.WriteLine("I am passionately learning C#");
             Console.ReadLine();
-        }
+            Console.WriteLine("It should end happily");
+            Console.ReadLine();
+       // Variables
+            int age = 23;
+            // age = age + 1;
+            age++;
+            Console.WriteLine($"Age: {age}");
+            Console.ReadLine();
+        
     }
+
+}
+
+   
 }
