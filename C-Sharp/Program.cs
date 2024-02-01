f56a1428493ac99504a0c8749febfae472b92b4e
Fatima Mughal <fatimamughal651@gmail.com>
2024-02-02 01:15:24 +0500

    String Concatenation

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -4,19 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I want to be a Successful Developer");
-            Console.WriteLine("I am passionately learning C#");
-            Console.ReadLine();
-            Console.WriteLine("It should end happily");
-            Console.ReadLine();
+
+            //Concatenation: Joining of two variables
+            string firstName = "Fatima";
+            string lastName = "Surraya";
+            string fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+
+            
        // Variables
             int age = 23;
             // age = age + 1;
             age++;
             Console.WriteLine($"Age: {age}");
             Console.ReadLine();
-        
-    }
+            string n = "Lahore";
+            Console.WriteLine($"City: {n}");
+
+            Console.WriteLine("I want to be a Successful Developer");
+            Console.WriteLine("I am passionately learning C#");
+            Console.ReadLine();
+            Console.WriteLine("It should end happily");
+            Console.ReadLine();
+        }
 
 }
 
